07393a20003d42828e6df01377b18bbb7c8b6d48
”Heorhii <hradinarpay@gmail.com>
2022-08-01 01:04:00 +0300

    add detail component

---
+++ src/app/confirmation-modal/confirmation-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-confirmation-modal',
+  templateUrl: './confirmation-modal.component.html',
+  styleUrls: ['./confirmation-modal.component.scss']
+})
+export class ConfirmationModalComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<ConfirmationModalComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+}
